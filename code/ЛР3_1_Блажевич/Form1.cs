60dc24cacce0e01411c612bfd0d40f4ed50b894b
egor1231245215213 <nemkov.yegor@bk.ru>
2025-11-19 13:54:16 +0700

    Add files via upload

---
+++ code/ЛР3_1_Блажевич/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Linq;
+using System.Text;
+
+namespace ЛР3_1_Блажевич
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double fahr = Convert.ToDouble(textBox1.Text);
+            double cels = 5.0 / 9 * (fahr - 32);
+            label2.Text = "Температура по Цельсию равна" + cels;
+
+
+        }
+    }
+}
